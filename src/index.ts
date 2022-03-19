d94160c6ff2835477620885a365b3cfae0d7cbc0
shuisheng <2274751790@qq.com>
2022-03-19 18:59:41 +0800

    fix: 修复error事件

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import staticService from 'koa-static'; // CJS: require('koa-static')
 
 import aliasOk from './app/alias';
 import { chalkINFO, chalkWRAN } from './app/chalkTip';
-import emitError from './app/handler/emit-error';
+import errorHandler from './app/handler/error-handle';
 import { connectDb } from './config/db';
 import verifyHandler from './middleware/verify.middleware';
 import useRoutes from './router/index';
@@ -47,7 +47,8 @@ app.use(async (ctx, next) => {
 app.use(
   bodyParser({
     onerror: (error, ctx) => {
-      emitError({ ctx, code: 500, error, message: 'body parse error' });
+      // 这个代码其实返回不到前端。
+      errorHandler({ ctx, code: 500, error, message: 'body parse error' });
     },
   })
 ); // 注意顺序，需要在所有路由加载前解析
@@ -73,7 +74,7 @@ connectDb()
     console.log(error);
   });
 
-app.on('error', emitError); // 全局错误处理
+app.on('error', errorHandler); // 全局错误处理
 
 app.listen(port, () => {
   console.log(chalkSUCCESS(`监听${port}端口成功!`));
