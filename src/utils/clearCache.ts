46a2a9c9cd6d253ec69fd335b0d8fde7713953c9
shuisheng <2274751790@qq.com>
2022-06-06 13:02:19 +0800

    fix: 修改内存任务

--- src/utils/clearCache.ts
+++ src/utils/clearCache.ts
@@ -2,17 +2,15 @@ import { Client } from 'ssh2';
 
 import { SSH_CONFIG } from '@/config/secret';
 
-// TODO部署后发现内存持续占用，使用ps aux | grep node命令发现是nuxt导致的
-// 应该整理nuxt的业务代码，清除监听事件等，现在方便就重启nuxt项目。
 const clearCacheCmd = () => {
   // 清除buff/cache命令
-  // return `echo 1 > /proc/sys/vm/drop_caches`;
-  return `pm2 restart nuxt-blog-client-null-3000`;
+  return `sync;echo 3 > /proc/sys/vm/drop_caches;`;
+  // return `pm2 restart nuxt-blog-client-null-3000`;
 };
 
 // 查看内存命令
 const showMemoryCmd = () => {
-  return `free`;
+  return `free -h`;
 };
 
 const startClearCacheJob = () => {
