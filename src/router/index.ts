7bfc7a64e7365e5918a48465d8699668487138fd
shuisheng <2274751790@qq.com>
2022-12-23 14:08:41 +0800

    feat: 优化接口

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,21 +2,27 @@ import fs from 'fs';
 
 import Router from 'koa-router';
 
+import { PROJECT_ENV, PROJECT_NAME } from '@/constant';
 import { chalkINFO, chalkERROR } from '@/utils/chalkTip';
 
 const router = new Router();
 
 export function loadAllRoutes(app) {
+  router.get('/', async (ctx, next) => {
+    ctx.body = {
+      message: `欢迎访问${PROJECT_NAME},当前环境是:${PROJECT_ENV},当前时间:${new Date().toLocaleString()}`,
+    };
+    await next();
+  });
+  app.use(router.routes()).use(router.allowedMethods()); // 每一个router都要配置routes()和allowedMethods()
+
   fs.readdirSync(__dirname).forEach((file) => {
     try {
       if (file === 'index.ts') return;
-      const linkRouter = require(`./${file}`).default;
-      app.use(linkRouter.routes()).use(linkRouter.allowedMethods());
-      // router.use('/front', linkRouter.routes()).use(linkRouter.allowedMethods());
-      router
-        .use('/admin', linkRouter.routes())
-        .use(linkRouter.allowedMethods());
-      app.use(router.routes()).use(router.allowedMethods()); // 这个有啥用？？？
+      const allRouter = require(`./${file}`).default;
+      app.use(allRouter.routes()).use(allRouter.allowedMethods()); // allRouter也要配置routes()和allowedMethods()
+      // router.use('/front', allRouter.routes()).use(allRouter.allowedMethods());
+      router.use('/admin', allRouter.routes()).use(allRouter.allowedMethods()); // admin的router也要配置routes()和allowedMethods()
       console.log(chalkINFO(`加载路由: ${file}`));
     } catch (error) {
       console.log(chalkERROR(`加载${file}路由出错:`));
